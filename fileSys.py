5dbcd66c9e4010c2867e19b754778811222d6897
Breno Gomes <breno.gomes@accountfy.com>
2021-10-23 17:04:55 -0300

    get_blocks_inf

--- fileSys.py
+++ fileSys.py
@@ -1,1 +1,15 @@
 
+class File():
+    def __init__(self,data_file):
+        self.name = data_file[0]
+        self.first_block = data_file[1]
+        self.quant_block = data_file[2]
+
+
+
+
+def get_blocks_inf(data_file):
+     content = data_file.readlines()
+     n_blocks = int(content[0])
+     ocp_blocks = int(content[1])
+     return (n_blocks, ocp_blocks)
--- pseudo-so.py
+++ pseudo-so.py
@@ -1,13 +1,22 @@
 from process import create_processes
 from process import Process
+from fileSys import get_blocks_inf
 REAL_TIME = 64
 USER = 960
+N_BLOCKS= 0
+OCP_BLOCKS =0 # blocos ocupados
+
 process_list = []
+files_list = []
+operations_list = []
 
 def run():
     files_so = open('files.txt','r')
     processes = open('processes.txt','r')
     process_list = create_processes(processes)
+    
+    N_BLOCKS,OCP_BLOCKS = get_blocks_inf(files_so)
+    
 
 
 run()
